36a400ea55f263d8b8bb637d2ccc3cd46b587e1b
Fathy Hosny <ali01120939266@gmail.com>
2026-03-17 15:02:13 +0200

    question01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question01
+
+            /*
+             What is a generic class? Why use generics?
+             */
+
+            //Answer:
+
+            // generic class is a class that can work with any data type
+            //It allows you to creat a class that can b use with different types of data without need to write multtiple versions of the class for all types
+
+            #endregion
         }
     }
 }
