2cd067c9944a11ab27bd44dd870cc98ad24eb641
Fathy Hosny <ali01120939266@gmail.com>
2026-03-17 15:05:43 +0200

    question02

--- Program.cs
+++ Program.cs
@@ -16,6 +16,37 @@
             //It allows you to creat a class that can b use with different types of data without need to write multtiple versions of the class for all types
 
             #endregion
+
+
+            #region question02
+
+            /*  
+            Write a generic class Container<T> with Add and Get methods.
+
+             */
+
+            //Answer:
+
+            /*
+             
+            class Container<T> 
+            {
+
+                private List<T> items = new List<T>();
+                public void Add(T item)
+                {
+                    items.Add(item);
+                }
+                public T Get(int index)
+                {
+                    return items[index];
+                }
+            
+             */
+
+
+            #endregion
+
         }
     }
 }
